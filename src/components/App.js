2e65b4ea5b823d4b866707a17710c85c74117293
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-01-08 22:22:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,24 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-//code here 
+  //code here 
+  const [active, setActive] = useState(true)
   
+  function handleClick() {
+    setActive(!active)
+  }
 
-  
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {/* assign value for button 1 */}
+        {active ? 'OFF' : 'ON'}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
         {/* assign value for button 2 */}
+        {active ? 'ON' : 'OFF'}
       </button>
     </div>
   );
